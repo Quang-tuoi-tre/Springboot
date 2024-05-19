dce2dd18f6e0437b04039257659ae4df65b1d294
Quang-tuoi-tre <honhatquang890@gmail.com>
2024-05-19 23:32:04 +0700

    hello

---
+++ src/test/java/honhatquang890/gmail/com/lab2/Lab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package honhatquang890.gmail.com.lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
